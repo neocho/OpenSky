26673ebe0b9d5af952bb7ba90b3c610e42c24781
Neo Cho <choneo.ch@gmail.com>
2021-09-13 03:40:03 -0400

    addy

--- helpers/cutAddress.js
+++ helpers/cutAddress.js
@@ -1,3 +1,7 @@
 export const cutAddress = (address) => {
-    return address?.substring(0, 4) + "..." + address?.substring(address.length-4, address.length);
+    if(address === undefined){
+        return;
+    }
+
+    return address.substring(0, 4) + "..." + address.substring(address.length-4, address.length);
 }
