b3ed0ce2fbb0938191464b53ed2d02e9fa52bed8
Neo Cho <choneo.ch@gmail.com>
2021-09-13 12:31:59 -0400

    view page

--- pages/explore.js
+++ pages/explore.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { ConnectWallet } from '../components/connect'
 import SyncWallet from '../hooks/useZksync'
 import Wallet from '../hooks/useWallet'
 import { ShowToast } from '../components/toast'
 import { showToast } from '../helpers/showToast'
 import { useIPFS } from '../hooks/useIPFS'
+import axios from 'axios'
 
 export default function Explore() {
     const { provider } = Wallet.useContainer();
@@ -14,19 +15,36 @@ export default function Explore() {
     const { decodeMultihash } = useIPFS();
     const [ collection, setCollection ] = useState([]); 
 
-    useEffect(() => {
-        const verified = nfts.verified; 
 
+    const getData = useCallback(async (verified) => {
         if(verified !== undefined) {
             let arr = []; 
 
             for(let [key, value] of Object.entries(verified)) {
-                value.url = decodeMultihash(value.contentHash);
-                arr.push(value);
+            
+                let nftData = {}
+
+                const url = decodeMultihash(value.contentHash);
+                const res = await axios.get(url);
+
+                nftData.id = value.id; 
+                nftData.symbol = value.symbol;
+                nftData.contentHash = value.contentHash; 
+                nftData.creatorId = value.creatorId; 
+                nftData.rest = res.data; 
+
+                arr.push(nftData);
             }
+
+            console.log(arr)
 
             setCollection(arr);
         }
+    }, []);
+
+    useEffect(async () => {
+        const verified = nfts.verified; 
+        getData(verified);
     }, [nfts]);
 
     return(
@@ -57,9 +75,9 @@ export default function Explore() {
                         {
                             collection && collection.map((nft, idx) => {
                                 return (
-                                    <div key={idx} className="flex flex-col rounded-2xl shadow-xl overflow-hidden border border-gray-300 flex-shrink-0 mr-8 mb-8 w-64">
+                                    <div key={idx} className="flex flex-col rounded-2xl shadow-xl overflow-hidden border border-gray-300 flex-shrink-0 mr-8 mb-8 w-64 h-96">
                                         <div className="flex justify-center items-center mt-6">
-                                            <img src={nft.url} alt="" width={180}></img>
+                                            <img src={nft.rest.file} alt="" width={180}></img>
                                         </div>
                                         <div className="flex flex-col items-start p-4 text-sm text-gray-500">
                                             <div> 
