70c765200f3e6bde97418cf0f1527d262d9e59ec
Neo Cho <choneo.ch@gmail.com>
2021-10-16 16:51:21 -0400

    footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col w-full h-36 border-t bg-black items-start pl-10 justify-center space-y-2">
       <h1 className="text-white font-bold text-xs leading-loose">
-        OpenSky 2021
+        OpenSky
       </h1>
       <div className="flex flex-row space-x-8 items-center">
         <a href="https://blockblock.io" target="_blank" rel="noreferrer">
